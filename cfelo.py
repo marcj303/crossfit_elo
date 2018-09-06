22dcc467551cd1632fb5e1b6b508d31c30feb5db
Marc Jones <marcj303@gmail.com>
2018-09-06 11:17:03 -0600

    Add 2018 Open ELO calculation
    Clea up the 2018 data and pass it through the ELO calculation.
    
    Updated K to (players/(players-1)) + 1, which comes out to
    about 2. This seems to keep the ELOs from spiraling out of
    controll. This allows the K to be used for 1200 players in
    the Open data and the 40 in the Games.
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>

--- cfelo.py
+++ cfelo.py
@@ -45,7 +45,7 @@ class ELOMatch:
 
     def calculateELOs(self):
         n = len(self.players)
-        K = 40 / (n - 1)
+        K = (n / (n - 1)) + 1
         print('players:', n, 'K:', K)
 
         for i in range(n):
@@ -78,8 +78,86 @@ class ELOMatch:
                 self.players[i].eloChange
 
 
+def processdata(df, workouts):
+    event = []
+
+    for i, w in enumerate(workouts):
+        df[w + '_ELO'] = 0
+
+        # Create a "match" for each event
+        event.append(ELOMatch())
+
+        # Iterate on each row to add the players (apply didn't work right?)
+        for index, row in df.iterrows():
+            # If the player placed then add the player to this match
+            # Check for NaN - there has to be a better python way?...
+            if (row[workouts[i]] == row[workouts[i]]):
+                event[i].addPlayer(row['NAME'], row[workouts[i]], row['ELO'])
+
+        event[i].calculateELOs()
+
+        # Get the data from each event and store it
+        # Also update each players current ELO
+        # Use apply instead of itterate.
+        # Per spec: You should never modify something you are iterating over.
+        # This is not guaranteed to work in all cases
+        # NOTE THIS DOESN'T WORK IF A PLAYER DROPS OUT.
+        # It does an apply for all NAMES
+        df[w + '_ELO'] = df.apply(lambda x: event[i].getELO(x['NAME']), axis=1)
+        df['ELO'] = df[w + '_ELO']
+    return df
+
+
 #
-# Crossfit 2018 ELO
+# Calculate 2018 Open ELO
+#
+df = pd.read_csv('./2018_open_top1200.csv',)
+print(df.head())
+print(df.info())
+
+workouts = ['18.1', '18.2', '18.2A', '18.3', '18.4', '18.5']
+renamecolumns = ['RANK', 'NAME', 'POINTS'] + workouts
+workouts_elo = [x + '_ELO' for x in workouts]
+
+#
+# Clean the 2018 Open Data
+#
+
+# clean up the column name cells
+df.columns = renamecolumns
+
+# Make Name a single long name
+df['NAME'] = df['NAME'].str.replace('ACCEPTED', '')
+df['NAME'] = df['NAME'].str.replace('TEAM', '')
+df['NAME'] = df['NAME'].str.replace('-', '')
+df['NAME'] = df['NAME'].str.replace('.', '')
+df['NAME'] = df['NAME'].str.replace(' ', '')
+
+# Use regex to get the name only
+df['NAME'] = df['NAME'].str.extract(r'^(\w+)', expand=False)
+
+# Use regex to get the first number (place) from the workout cell,
+# make it into a number or a NaN
+for w in workouts:
+    df[w] = df[w].str.extract(r'^(\d+)', expand=False)
+    df[w] = df[w].apply(pd.to_numeric, errors='coerce')
+
+# Everyon starts with an ELO of 1500
+df['ELO'] = 1500
+print(df.head())
+
+# Calculate the ELOs
+df = processdata(df, workouts)
+
+# Save the results
+df.to_csv('./cf_open_elo_results.csv')
+
+# Get the ELOs to seed the next round
+df1 = df[['NAME', 'ELO']]
+print(df1.head())
+
+#
+# Crossfit 2018 Games ELO
 #
 df = pd.read_csv('./2018_men_leaderboard.csv',)
 
@@ -92,25 +170,20 @@ workouts = ['CRIT', '30 MUSCLE-UPS',
             'MADISON TRIPLUS', 'CHAOS', 'BICOUPLET 2',
             'BICOUPLET 1', 'TWO-STROKE PULL', 'HANDSTAND WALK',
             'AENEAS']
-
 renamecolumns = ['RANK', 'NAME', 'POINTS'] + workouts
-
 workouts_elo = [x + '_ELO' for x in workouts]
-
-
-#
-# Clean the Data
-#
 
 # clean up the column name cells
 df.columns = renamecolumns
 
-# Cleanup the NAME cells
+# Cleanup the NAME cells. Get everything on the same line
 df['NAME'] = df['NAME'].str.replace('\n', ' ')
 
 # Use regex to get the first and last name only
 df['NAME'] = df['NAME'].str.extract(r'^(\w+\s\w+)', expand=False)
-print(df['NAME'])
+
+# Make Name a single long name
+df['NAME'] = df['NAME'].str.replace(' ', '')
 
 # Use regex to get the first number (place) from the workout cell,
 # make it into a number or a NaN
@@ -118,50 +191,19 @@ for w in workouts:
     df[w] = df[w].str.extract(r'^(\d+)', expand=False)
     df[w] = df[w].apply(pd.to_numeric, errors='coerce')
 
-# Everyon starts with an ELO of 1500
-df['ELO'] = 1500
+# Everyone starts with an ELO from the open
+df = pd.merge(df, df1, on='NAME')
+df['ELO_IN'] = df['ELO']
+print(df.head())
 
+# Calculate the ELOs
+df = processdata(df, workouts)
 
-#
-# Process the data
-#
-event = []
-
-for i, w in enumerate(workouts):
-    df[w + '_ELO'] = 0
-
-    # Create a "match" for each event
-    event.append(ELOMatch())
-
-    # Iterate on each row to add the players (apply didn't work right?)
-    for index, row in df.iterrows():
-        # If the player placed then add the player to this match
-        # Check for NaN - there has to be a better python way?...
-        if (row[workouts[i]] == row[workouts[i]]):
-            event[i].addPlayer(row['NAME'], row[workouts[i]], row['ELO'])
-
-    event[i].calculateELOs()
-
-    # Get the data from each event and store it
-    # Also update each players current ELO
-    # Use apply instead of itterate.
-    # Per spec: You should never modify something you are iterating over.
-    # This is not guaranteed to work in all cases
-    df[w + '_ELO'] = df.apply(lambda x: event[i].getELO(x['NAME']), axis=1)
-    df['ELO'] = df[w + '_ELO']
+# Save the dataframe to a file
+df.to_csv('./cf_elo_results.csv')
 
 
 #
 # Plot the data
 #
-
-# gca stands for 'get current axis'
-# ax = plt.gca()
-# df.plot(kind='line', x='NAME', y=workouts_elo, ax=ax)
 # plt.show()
-
-
-#
-# Save the dataframe to a file
-#
-df.to_csv('./cf_elo_results.csv')
